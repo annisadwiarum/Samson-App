64308c368144a6b7f9086ae43fa4085c77d53903
annisadwiarum <annisadwiarum710@gmail.com>
2024-03-09 16:59:27 +0700

    update

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -7,7 +7,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST(request: Request) {
   const { username } = await request.json();
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "annisadwiarum710@gmail.com",
     to: "annisaalfatih1453@gmail.com",
     subject: "hello world",
     react: EmailSender({
